358d05f9664c3bc209012364e757469fc21a187f
spoorn <spookump@gmail.com>
2022-04-01 13:48:51 -0700

    Initial local commit

---
+++ src/main/java/org/spoorn/qolfixes/Qolfixes.java
@@ -0,0 +1,10 @@
+package org.spoorn.qolfixes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Qolfixes implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/spoorn/qolfixes/client/QolfixesClient.java
@@ -0,0 +1,13 @@
+package org.spoorn.qolfixes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class QolfixesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
